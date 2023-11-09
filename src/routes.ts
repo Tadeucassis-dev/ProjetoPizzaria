f182eb8b1806dad3ffff7cd073f6d09c971b3680
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2023-11-09 10:08:03 -0300

    finish orders

--- src/routes.ts
+++ src/routes.ts
@@ -20,6 +20,9 @@ import { AddItemService } from './services/order/AddItemService';
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrdersController } from './controllers/order/ListOrderController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 const router = Router();
 
@@ -45,7 +48,14 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
-router.post('/order/add', isAuthenticated, new AddItemController().handle )
-router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle )
-router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle )
+
 export { router };
